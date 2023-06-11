74944c2e7e8a42ce21a5886f96674c77d8dd918b
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-06-11 21:53:32 +0300

    add  Primary Bean

--- src/test/java/com/anastasiiaTkachuk/spring6DI/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/com/anastasiiaTkachuk/spring6DI/controllers/ConstructorInjectedControllerTest.java
@@ -3,6 +3,7 @@ package com.anastasiiaTkachuk.spring6DI.controllers;
 import com.anastasiiaTkachuk.spring6DI.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ConstructorInjectedControllerTest {
 
+    @Autowired
     ConstructorInjectedController constructorInjectedController;
 
     @Test
